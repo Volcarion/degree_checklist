17f6fdcf50577f3e5e5c40b3fa9ffedf391953db
Josh Tarrant <allegoricsnake@msn.com>
2023-09-24 22:29:29 -0500

    some changes still not right

--- degree_checklist/admin.py
+++ degree_checklist/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from Assignment2.degree_checklist.models import Course, Student, StudentCourse, StudentDegree, DegreePlan
 
 # Register your models here.
+admin.site.register(Student, Course, StudentCourse, StudentDegree, DegreePlan)
