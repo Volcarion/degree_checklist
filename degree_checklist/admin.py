6c9b4453fba8e5e3bf703f8c6d0ef50f0c74bb3a
Josh Tarrant <allegoricsnake@msn.com>
2023-10-09 14:38:26 -0500

    got the database tables to show up and added some data

--- degree_checklist/admin.py
+++ degree_checklist/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from Assignment2.degree_checklist.models import Course, Student, StudentCourse, StudentDegree, DegreePlan
+from .models import Course, Student, StudentCourse, StudentDegree, DegreePlan
 
 # Register your models here.
-admin.site.register(Student, Course, StudentCourse, StudentDegree, DegreePlan)
+# admin.site.register(Student, Course, StudentCourse, StudentDegree, DegreePlan)
--- degree_checklist/models.py
+++ degree_checklist/models.py
@@ -2,30 +2,30 @@ from django.db import models
 
 
 class Student(models.Model):
-    StudentId = models.IntegerField()
+    StudentId = models.CharField(max_length=250)
     Name = models.CharField(max_length=250)
     BirthDate = models.DateTimeField()
     Address = models.CharField(max_length=250)
 
 
 class StudentDegree(models.Model):
-    StudentId = models.IntegerField()
+    StudentId = models.CharField(max_length=250)
     DegreeId = models.IntegerField()
 
 
 class DegreePlan(models.Model):
     DegreeId = models.IntegerField()
     DegreeName = models.CharField(max_length=250)
-    CourseId1 = models.IntegerField()
-    CourseId2 = models.IntegerField()
+    CourseId1 = models.CharField(max_length=250)
+    CourseId2 = models.CharField(max_length=250)
 
 
 class StudentCourse(models.Model):
-    StudentId = models.IntegerField()
-    CourseId = models.IntegerField()
+    StudentId = models.CharField(max_length=250)
+    CourseId = models.CharField(max_length=250)
 
 
 class Course(models.Model):
-    CourseId = models.IntegerField()
+    CourseId = models.CharField(max_length=250)
     CourseName = models.CharField(max_length=250)
-    PreRequisiteCourseId = models.IntegerField()
+    PreRequisiteCourseId = models.CharField(max_length=250)
